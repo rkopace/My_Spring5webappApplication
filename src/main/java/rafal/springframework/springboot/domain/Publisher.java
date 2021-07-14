0dcfcdff8a78e1c0f901bac12864bd8404ffb113
rkopace <greenball@wp.pl>
2021-07-14 12:21:03 -0400

    created Publisher entity

---
+++ src/main/java/rafal/springframework/springboot/domain/Publisher.java
@@ -0,0 +1,67 @@
+package rafal.springframework.springboot.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+    
+    private String name;
+    private String addressLine1;
+    private String city;
+    private String state;
+    private String zip;
+    
+    private Set<Book> books = new HashSet<>();
+ 
+    
+	public Publisher() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Publisher [id=" + id + ", name=" + name + ", addressLine1=" + addressLine1 + ", city=" + city
+				+ ", state=" + state + ", zip=" + zip + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Publisher other = (Publisher) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+	
+	
+    
+    
+    
+}
